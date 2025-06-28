671137cb2a5f1b31f1264bbb4c2309aabd192e0b
najatul6 <mdnajatulislam.develop@gmail.com>
2025-06-28 09:16:09 +0600

    feat: update App component to display birthday balloons and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="happy">
+        <div class="balloon balloon1 balloon-anim1"></div>
+        <div class="balloon balloon2 balloon-anim6"></div>
+        <div class="balloon balloon3 balloon-anim3"></div>
+        <div class="balloon balloon4 balloon-anim4"></div>
+        <div class="balloon balloon5 balloon-anim5"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="birthday">
+        <div class="balloon balloon3 balloon-anim6"></div>
+        <div class="balloon balloon1 balloon-anim2"></div>
+        <div class="balloon balloon4 balloon-anim1"></div>
+        <div class="balloon balloon2 balloon-anim6"></div>
+        <div class="balloon balloon1 balloon-anim4"></div>
+        <div class="balloon balloon3 balloon-anim5"></div>
+        <div class="balloon balloon2 balloon-anim1"></div>
+        <div class="balloon balloon4 balloon-anim6"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class="name">
+        My Dear
+      </div>
     </>
   )
 }
